0e787c9951ba526b609db266eae9bc7da7163966
RedoRojo <cristhiabnvictorrm@gmail.com>
2024-09-25 23:21:55 -0400

    Script to make an historical of test implemented

--- script.ts
+++ script.ts
@@ -1,4 +1,5 @@
 import { exec } from 'child_process';
+import * as fs from 'fs';
 
 const command = 'npm run test-export-json'; 
 
@@ -13,3 +14,43 @@ exec(command, (error, stdout, stderr) => {
     }
     console.log(`stdout: ${stdout}`);
 });
+
+
+const readJSONFile = (filePath: string): any => {
+  const rawData = fs.readFileSync(filePath, 'utf-8');
+  return JSON.parse(rawData);
+};
+
+const writeJSONFile = (filePath: string, data: any): void => {
+  const jsonString = JSON.stringify(data, null, 2);
+  fs.writeFileSync(filePath, jsonString, 'utf-8');
+};
+
+const extractAndAddObject = (inputFile: string, outputFile: string) => {
+
+  const jsonData = readJSONFile(inputFile);
+
+  const value1 = jsonData.numPassedTests;
+  const value2 = jsonData.numTotalTests;
+  const value3 = jsonData.startTime;
+
+  const newObject = {
+    numPassedTests: value1,
+    numTotalTests: value2,
+    timestamp: value3
+  };
+
+  const outputData = readJSONFile(outputFile);
+
+  outputData.push(newObject);
+
+  writeJSONFile(outputFile, outputData);
+
+  console.log('New object added successfully!');
+};
+
+const inputFilePath = './report.json'; 
+const outputFilePath = './tdd_log.json';
+
+extractAndAddObject(inputFilePath, outputFilePath);
+
